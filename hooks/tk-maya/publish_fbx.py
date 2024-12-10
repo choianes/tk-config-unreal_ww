dce7e078f094a39c203482e9bbc882dd7c406341
choianes <choianes118@gmail.com>
2024-12-10 17:01:30 +0900

    maya에서 샷그리드에 pub하면서 로컬 pub경로에 fbx파일 저장

--- hooks/tk-maya/publish_fbx.py
+++
@@ -1,9 +0,0 @@
-import maya.cmds as cmds
-import os
-
-def export_fbx(output_path):
-    # FBX export 설정
-    cmds.loadPlugin("fbxmaya", quiet=True)
-    cmds.file(rename=output_path)
-    cmds.file(force=True, options="v=0", type="FBX export", exportAll=True)
-    print(f"Exported FBX to {output_path}")
